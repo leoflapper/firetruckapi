f6a1cfd1aa9c9187b6831eedee50bb5cba5daa78
Leo Flapper <dominicus1991@gmail.com>
2016-08-05 14:57:08 +0200

    Fixed the example

--- example.php
+++ example.php
@@ -1,18 +1,17 @@
 <?php
 
-
 include 'vendor/autoload.php';
 
-$client = new FireTruck\API\Client('a2047ea0b10a08dac0d2fa6bcb14d6171a7cf584');
+$client = new FireTruck\API\Client('APIKEY');
 $client->setVerify(false);
 
 
 try {
-    $response = $client->get('domains/5791c9ef0f9d1f0001bdb56a');
+    $response = $client->get('domains/DOMAINHASH');
 } catch(FireTruck\API\Exception\FiretruckAPIException $e) {
     $response = $e;
 }
 
 echo '<pre>'; print_r($response->getBody()); echo '</pre>'; die();
 
-
+?>
